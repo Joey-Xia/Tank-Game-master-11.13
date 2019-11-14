4e9725b23007b28b5938c663d1d8b17909b8609d
Ziyi Xia <joey@ip-75-106.wireless.oberlin.edu>
2019-11-13 21:13:04 -0500

    all files

---
+++ core/src/com/tank_game/game/Game.java
@@ -0,0 +1,47 @@
+package com.tank_game.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Game extends ApplicationAdapter {
+	private SpriteBatch batch;
+	private OrthographicCamera camera;
+	private Tank player;
+
+	public void create () {
+		// Loads camera
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 800);
+
+		// Create Batch
+		batch = new SpriteBatch();
+		batch.setProjectionMatrix(camera.combined);
+
+		// Creates Player
+		player = new Tank("tank.jpg", "canon.jpg","bullet.jpg", 400, 400);
+		}
+
+	public void render () {
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		// Esc to exit
+		if(Gdx.input.isKeyPressed(Input.Keys.ESCAPE)) System.exit(0);
+
+		// Runs through the players actions
+		player.step();
+
+		// Updates the camera
+		camera.update();
+	}
+
+	public void dispose () {
+	}
+}
---
+++ core/src/com/tank_game/game/Tank.java
@@ -0,0 +1,126 @@
+package com.tank_game.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
+
+import java.util.ArrayList;
+
+public class Tank extends ApplicationAdapter {
+    private SpriteBatch batch = new SpriteBatch();
+
+    public Texture tankImg;
+    private Texture canonImg;
+    public Texture bulletImg;
+    public Rectangle collision;
+
+    //tank's bullets array list
+    public ArrayList<Bullet> bullets;
+
+    int timer = 0; // For counting up
+    int bullet_cooldown = 50; // To limit the amount of bullets a tank can shoot each unit time
+
+    public Tank(String tankImgPath, String canonImgPath, String bulletImgPath, int x, int y) {
+        tankImg = new Texture(Gdx.files.internal(tankImgPath));
+        canonImg = new Texture(Gdx.files.internal(canonImgPath));
+        bulletImg = new Texture(Gdx.files.internal(bulletImgPath));
+        collision = new Rectangle();
+        collision.x = x;
+        collision.y = y;
+        collision.width = 64;
+        collision.height = 64;
+        //initialize bullets
+        bullets = new ArrayList<Bullet>();
+    }
+
+    public void step() {
+        movement();
+        mouse_point();
+        bullet_movement();
+        timer++;
+        bullet_cooldown++;
+
+        // Draws all the textures in the game
+        batch.begin();
+        if (!bullets.isEmpty()){
+            for (Bullet bullet: bullets) {
+                batch.draw(bulletImg, bullet.collision.x, bullet.collision.y, 10, 10);
+            }
+        }
+        batch.draw(tankImg, collision.x, collision.y);
+        batch.draw(canonImg, collision.x + 24, collision.y + 24, 8, 8, 16, 64,
+        1, 1, mouse_point(), 0, 0, 16, 64, false, false);
+
+        batch.end();
+    }
+
+    public void movement() {
+        // Keyboard Movement
+        if(Gdx.input.isKeyPressed(Input.Keys.A)) collision.x -= 200 * Gdx.graphics.getDeltaTime();
+        if(Gdx.input.isKeyPressed(Input.Keys.D)) collision.x += 200 * Gdx.graphics.getDeltaTime();
+        if(Gdx.input.isKeyPressed(Input.Keys.W)) collision.y += 200 * Gdx.graphics.getDeltaTime();
+        if(Gdx.input.isKeyPressed(Input.Keys.S)) collision.y -= 200 * Gdx.graphics.getDeltaTime();
+
+        // Restricts the tank to go outside of the window
+        if(collision.x < 0) collision.x = 0;
+        if(collision.x > 800 - 64) collision.x = 800 - 64;
+        if(collision.y < 0) collision.y = 0;
+        if(collision.y > 800 - 64) collision.y = 800 - 64;
+    }
+
+    public void bullet_movement() {
+        //update the location of all the bullet
+        ArrayList<Bullet> new_bullets = new ArrayList<Bullet>();
+        for (Bullet bullet: bullets){
+            bullet.collision.x -= 300 * Math.sin(Math.toRadians(bullet.angle)) * Gdx.graphics.getDeltaTime();
+            bullet.collision.y += 300 * Math.cos(Math.toRadians(bullet.angle)) * Gdx.graphics.getDeltaTime();
+            System.out.println("angle :"+bullet.angle);
+            System.out.println("x" + bullet.collision.x);
+            System.out.println("y" + bullet.collision.y);
+            if (bullet.collision.x < 800 && bullet.collision.x > 0 && bullet.collision.y < 800 && bullet.collision.y > 0){
+                new_bullets.add(bullet);
+            }
+        }
+        bullets = new_bullets;
+
+        // Keyboard Shoot Bullet
+        if(Gdx.input.isKeyPressed(Input.Keys.SPACE) && bullet_cooldown >= 50){
+            float angle_temp = mouse_point();
+            float x_temp = collision.x + 32 - 64 * (float)Math.sin(Math.toRadians(angle_temp));
+            float y_temp = collision.y + 32 + 64 * (float)Math.cos(Math.toRadians(angle_temp));
+            Bullet bullet = new Bullet(x_temp, y_temp , angle_temp);
+            bullets.add(bullet);
+            System.out.println("angle: " + bullet.angle);
+            bullet_cooldown = 0;
+        }
+    }
+
+    public float mouse_point() {
+        // Holds the mouse coordinates
+        Vector3 mousePos = new Vector3();
+        if (timer == 50) {
+            System.out.println("x: " + collision.x + ", y: " + collision.y);
+            System.out.println("Mouse x: " + Gdx.input.getX() + ", Mouse y: " + Gdx.input.getY());
+            timer = 0;
+        }
+
+        float xDif = collision.x - Gdx.input.getX();
+        float yDif = collision.y - Gdx.input.getY();
+        double angle = Math.toDegrees(Math.atan(xDif / yDif));
+        if (yDif <= 0) {
+            angle += 180;
+        }
+        //change the angle range from -90-270, to 0-360.
+        if (angle < 0) {
+            angle = 360 + angle;
+        }
+
+        // System.out.println(angle);
+        return (float)angle;
+    }
+}
